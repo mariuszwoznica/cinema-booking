c02b074f8eb5765d0713088807a82b2f61e45584
mariusz <mariusz.adam.woznica@gmail.com>
2025-12-06 20:26:59 +0100

    add logging decorators for command and query handlers

---
+++ src/Common/CinemaBooking.Common.Infrastructure/Logging/Decorators/LoggingCommandHandlerDecorator.cs
@@ -0,0 +1,24 @@
+﻿using CinemaBooking.Common.Abstractions.Commands;
+using Microsoft.Extensions.Logging;
+
+namespace CinemaBooking.Common.Infrastructure.Logging.Decorators;
+
+internal sealed class LoggingCommandHandlerDecorator<TCommand>(
+    ICommandHandler<TCommand> decorated, 
+    ILogger<LoggingCommandHandlerDecorator<TCommand>> logger) : ICommandHandler<TCommand> 
+        where TCommand : class, ICommand
+{
+    private readonly ICommandHandler<TCommand> _decorated = decorated;
+    private readonly ILogger<LoggingCommandHandlerDecorator<TCommand>> _logger = logger 
+        ?? throw new ArgumentNullException(nameof(logger));
+    
+    public async Task HandleAsync(TCommand command, CancellationToken cancellationToken)
+    {
+        var commandTypeName = typeof(TCommand).Name;
+        _logger.LogInformation("Handling a command {CommandTypeName}", commandTypeName);
+        
+        await _decorated.HandleAsync(command, cancellationToken);
+        
+        _logger.LogInformation("Handled a command {CommandTypeName}", commandTypeName);
+    }
+}
---
+++ src/Common/CinemaBooking.Common.Infrastructure/Logging/Decorators/LoggingQueryHandlerDecorator.cs
@@ -0,0 +1,26 @@
+﻿using CinemaBooking.Common.Abstractions.Queries;
+using Microsoft.Extensions.Logging;
+
+namespace CinemaBooking.Common.Infrastructure.Logging.Decorators;
+
+internal sealed class LoggingQueryHandlerDecorator<TQuery, TResponse>(
+    IQueryHandler<TQuery, TResponse> decorated, 
+    ILogger<LoggingQueryHandlerDecorator<TQuery, TResponse>> logger) : IQueryHandler<TQuery, TResponse> 
+        where TQuery : class, IQuery<TResponse>
+{
+    private readonly IQueryHandler<TQuery, TResponse> _decorated = decorated;
+    private readonly ILogger<LoggingQueryHandlerDecorator<TQuery, TResponse>> _logger = logger
+        ?? throw new ArgumentNullException(nameof(logger));
+    
+    public async Task<TResponse> HandleAsync(TQuery query, CancellationToken cancellationToken)
+    {
+        var queryTypeName = typeof(TQuery).Name;
+        _logger.LogInformation("Handling a query {QueryTypeName}", queryTypeName);
+        
+        var result = await _decorated.HandleAsync(query, cancellationToken);
+        
+        _logger.LogInformation("Handled a query {QueryTypeName}", queryTypeName);
+        
+        return result;
+    }
+}
