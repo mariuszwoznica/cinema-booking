cd6c89db37a9b683ef34d1eff35db6bc43fa5d10
mariusz <mariusz.adam.woznica@gmail.com>
2026-01-11 18:30:23 +0100

    add migrations

---
+++ src/Modules/Cinemas/CinemaBooking.Modules.Cinemas.Core/Data/Migrations/20260111172403_AddCinemas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaBooking.Modules.Cinemas.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCinemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "cinemas");
+
+            migrationBuilder.CreateTable(
+                name: "Cinemas",
+                schema: "cinemas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "jsonb", nullable: false),
+                    Screens = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cinemas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cinemas",
+                schema: "cinemas");
+        }
+    }
+}
